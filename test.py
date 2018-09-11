465d829d20d7aac65bb174e4f0128be5edda64c3
Roujia Wang <rosesharpaywang@RoujiamatoMacBook-Pro.local>
2018-09-11 15:57:48 -0400

    test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+def main():
+    result = add_three(1, 3, 5)
+    divide_three(result)
+
+def add_three(v1, v2, v3):
+    p = v1 + v2 + v3
+    return p
+
+def divide_three(v1):
+    p = v1 / 3
+    print(p)
+
+if __name__ = "__main__"
+    main()
+
+
+    
